dfdee075c642c00da81d5b8130ec4a8f86dcd13a
Anderson <anderson.bhilly@gmail.com>
2022-09-12 15:20:12 -0300

    first commit

---
+++ varias.py
@@ -0,0 +1,6 @@
+import PySimpleGUI as sg
+
+# Criar as janelas e estilos(layout)
+# Criar  as janelas iniciais
+# Criar um loop de leituras de eventos
+# Lógica de o que deve acontecer ao clicar os botões
